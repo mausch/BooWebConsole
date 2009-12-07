baabb8614f66493cb2af2c9ff8b3ae3a7d677bc9
mausch <mauricioscheffer@gmail.com>
2009-12-06 22:50:07 -0300

    starting with autocomplete

---
+++ BooWebConsole/ControllerFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace BooWebConsole {
+    public class ControllerFactory : IHttpHandlerFactory {
+        public IHttpHandler GetHandler(HttpContext context, string requestType, string url, string pathTranslated) {
+            var name = context.Request.RawUrl.Split('/').Last().Split('?')[0].Split('.')[0];
+            name = name.ToUpperInvariant().First() + new string(name.ToLowerInvariant().Skip(1).ToArray());
+            return (IHttpHandler) Activator.CreateInstance(Type.GetType(string.Format("BooWebConsole.{0}Controller", name)));
+        }
+
+        public void ReleaseHandler(IHttpHandler handler) {}
+    }
+}
---
+++ BooWebConsole/SuggestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using Boo.Lang.Interpreter;
+using MiniMVC;
+
+namespace BooWebConsole {
+    public class SuggestController: Controller {
+        public override IResult Execute(HttpContext context) {
+            var interpreter = new InteractiveInterpreter();
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+                interpreter.References.Add(a);
+            var suggestions = interpreter.SuggestCodeCompletion(context.Request["code"])
+                .Select(c => c.Name)
+                .Distinct()
+                .OrderBy(x => x)
+                .Select(c => string.Format("'{0}'", c))
+                .ToArray();
+            return new RawResult(string.Format("[{0}]", string.Join(",", suggestions))) {
+                ContentType = "application/json"
+            };
+        }
+    }
+}
